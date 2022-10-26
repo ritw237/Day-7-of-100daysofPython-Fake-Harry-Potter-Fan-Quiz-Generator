574de0b5219334b6ad7cfb3fcd987122635a19f7
Ritwik Srivastava <ritwikdbz@gmail.com>
2022-10-26 13:33:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+fan = input("Are you a true fan of Harry Potter?")
+print()
+if fan == "yes":
+  print("Then prepared to get tested on this Harry Potter Fan Quiz!!!")
+  print()
+  q1 = input("Q1: Was Harry a horcrux himself?")
+  if q1 =="yes":
+    print("You are a true fan!")
+  else:
+    print("You know nothing! Read novels please!")
+
+else:
+  print("Go away! You are clearly not a Potterhead!")
